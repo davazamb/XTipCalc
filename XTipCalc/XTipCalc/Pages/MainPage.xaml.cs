de8681315fb510608b892398901cc4b00ba8e092
David Zambrano <davazamb10@outlook.com>
2017-02-26 09:20:47 -0400

    Models TipInfo

--- XTipCalc/XTipCalc/Pages/MainPage.xaml.cs
+++ XTipCalc/XTipCalc/Pages/MainPage.xaml.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using XTipCalc.Models;
 
 namespace XTipCalc.Pages
 {
     public partial class MainPage : ContentPage
     {
+        TipInfo info = new TipInfo()
+        {
+            TipPercent = 15
+        };
         public MainPage()
         {
             InitializeComponent();
