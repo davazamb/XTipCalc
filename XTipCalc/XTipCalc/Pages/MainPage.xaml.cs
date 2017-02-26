063a955ac78ef912c8eb8574a4bad98da63d4a6b
David Zambrano <davazamb10@outlook.com>
2017-02-26 09:27:41 -0400

    MainPage codebehind. Example Campus End.

--- XTipCalc/XTipCalc/Pages/MainPage.xaml.cs
+++ XTipCalc/XTipCalc/Pages/MainPage.xaml.cs
@@ -12,11 +12,58 @@ namespace XTipCalc.Pages
     {
         TipInfo info = new TipInfo()
         {
-            TipPercent = 15
+            TipPercent = 12
         };
         public MainPage()
         {
             InitializeComponent();
+
+            TipPercent.Text = info.TipPercent.ToString();
+            TipPercentSlider.Value = Convert.ToDouble(info.TipPercent);
+
+            info.TipValueChanged += (sender, e) =>
+            {
+                TipValue.Text = info.TipValue.ToString();
+                Total.Text = (info.TipValue + info.Total).ToString();
+            };
+
+            ReceiptTotal.TextChanged += (sender, e) =>
+            {
+                info.Total = Parse(ReceiptTotal.Text);
+            };
+
+            TipPercent.TextChanged += (sender, e) =>
+            {
+                info.TipPercent = Parse(TipPercent.Text);
+            };
+
+            Subtotal.TextChanged += (sender, e) =>
+            {
+                info.Subtotal = Parse(Subtotal.Text);
+            };
+
+            TipPercentSlider.ValueChanged += (sender, e) =>
+            {
+                TipPercent.Text = e.NewValue.ToString();
+                TipPercentSlider.Value = e.NewValue;
+            };
+        }
+
+        static decimal Parse(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return 0m;
+
+            try
+            {
+                return Convert.ToDecimal(text);
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e.Message);
+
+                return 0m;
+            }
         }
     }
 }
